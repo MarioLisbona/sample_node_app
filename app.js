5610c15f996a808b1545710cd6ea171a85d8f9ca
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-04-05 10:54:43 +1100

    Fix bug

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send('<h1 style="color: blue; font-size: 48px;">Node Application</h1>');
-  res.send('<h2 style="color: red; font-size: 24px;">Coda Marie Art</h2>');
+  res.send(
+    '<h1 style="color: blue; font-size: 48px;">Node Application</h1> <h2 style="color: red; font-size: 24px;">Coda Marie Art</h2>'
+  );
 });
 
 app.listen(3000, () => {
