56d248c389fd96aecacd61589b8d5579f7c5c632
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-04-05 10:15:39 +1100

    Change port to 3000 locally

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ app.get("/", (req, res) => {
   res.send('<h1 style="color: blue; font-size: 48px;">Node Application</h1>');
 });
 
-app.listen(8080, () => {
-  console.log("App listening on port http://localhost:8080");
+app.listen(3000, () => {
+  console.log("App listening on port http://localhost:3000");
 });
