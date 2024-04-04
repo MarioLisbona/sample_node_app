77b6b2e75747d7bf81ee30827c0436e206f86d4d
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2024-04-05 10:51:15 +1100

    add h2

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.get("/", (req, res) => {
   res.send('<h1 style="color: blue; font-size: 48px;">Node Application</h1>');
-  res.send('<h1 style="color: red; font-size: 24px;">Coda Marie Art</h1>');
+  res.send('<h2 style="color: red; font-size: 24px;">Coda Marie Art</h2>');
 });
 
 app.listen(3000, () => {
